5f6c70fbc86d2341043de4eab848a50253b88176
Matthias Grob <maetugr@gmail.com>
2023-07-13 19:20:44 +0200

    FlightModeManager: make sure no internal flight task is running when at the same time as an external mode

--- src/modules/flight_mode_manager/FlightModeManager.cpp
+++ src/modules/flight_mode_manager/FlightModeManager.cpp
@@ -137,7 +137,8 @@ void FlightModeManager::updateParams()
 void FlightModeManager::start_flight_task()
 {
 	// Do not run any flight task for VTOLs in fixed-wing mode
-	if (_vehicle_status_sub.get().vehicle_type == vehicle_status_s::VEHICLE_TYPE_FIXED_WING) {
+	if (_vehicle_status_sub.get().vehicle_type == vehicle_status_s::VEHICLE_TYPE_FIXED_WING ||
+	    _vehicle_status_sub.get().nav_state >= vehicle_status_s::NAVIGATION_STATE_EXTERNAL1) {
 		switchTask(FlightTaskIndex::None);
 		return;
 	}
